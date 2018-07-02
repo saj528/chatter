0ba9aeaea6a70eaf1fd007d4aabc2bff538af10f
saj528 <ajmarksboy@gmail.com>
2018-07-02 17:58:16 +0000

    signup.scss created. added css to signup page, added import for partials folder to application.scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
